dd4b344bf74e4c52c1f9c2006c5246a5fd9a1fbd
Omar Faruque Tuhin <omartuhin@pathao.com>
2023-06-27 17:35:20 +0600

    updated styling

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var carpraze_contact_form = (function() {
+var carpraze_contact_form = (function () {
     // Define the CSS
     var css = `
     .cp-modal {
@@ -50,7 +50,6 @@ var carpraze_contact_form = (function() {
     #cp-appraisal-form {
         display: grid;
         gap: 1em;
-        max-width: 300px;
         margin: auto;
     }
 
@@ -86,6 +85,15 @@ var carpraze_contact_form = (function() {
     #cp-appraisal-form input[type=submit]:hover {
         background-color: #45a049;
     }
+    
+    .cp-modal-footer p {
+        text-align: center;
+    }
+    
+    .cp-footer-logo {
+        vertical-align: middle;
+        height: 40px;
+    }
     `;
 
     // Create a <style> element
@@ -103,25 +111,25 @@ var carpraze_contact_form = (function() {
     <div class="cp-modal" id="cp-appraisal-modal">
         <div class="cp-modal-content">
             <span class="cp-close-button">×</span>
-            <p>Carpraze appraisal request!</p>
-            <div>
+            <div class="cp-modal-header">
+                <h2>Find out what your trade in is worth</h2>
+            </div>
+            <div class="cp-modal-description">
+                <p>Fill out the form below and receive a Firm Offer from a Real Person (Not a computer-generated estimate).  Once your input your information, you will receive a text message and e-mail.  To get started you will need important information about your vehicle.</p>
+            </div>
+            <div class="cp-modal-form">
                 <form id="cp-appraisal-form" method="post">
-                    <label for="first_name">First Name</label>
                     <input type="text" id="first_name" name="first_name" placeholder="First Name" required>
-
-                    <label for="last_name">Last Name</label>
                     <input type="text" id="last_name" name="last_name" placeholder="Last Name" required>
-
-                    <label for="email">Email</label>
                     <input type="email" id="email" name="email" placeholder="Email" required>
-
-                    <label for="phone">Phone</label>
                     <input type="text" id="phone" name="phone" placeholder="Phone" required>
-
-                    <input type="hidden" id="form_type" name="form_type" value="customerInput">
-
                     <input type="submit" value="Submit">
                 </form>
+            </div>
+            <div class="cp-modal-footer">
+                <p>
+                    <img class="cp-footer-logo" src="https://www.carza.ca/hosted/images/39/a73dbfe37740fb9d4b78950bbcdb0f/Screen_Shot_2021-05-28_at_1.11.13_PM-removebg-preview.png" />
+                </p>
             </div>
         </div>
     </div>
@@ -129,71 +137,73 @@ var carpraze_contact_form = (function() {
 
     widgetContainer.innerHTML = html;
 
+    function handleFormOnload() {
+        if (!(this.status === 200 || this.status === 201)) {
+            console.error("There was an error with the request.");
+            alert("There was an error with the request.")
+            return;
+        }
+
+        var response = JSON.parse(this.responseText);
+        // Create a success message element
+        var successMessage = document.createElement("div");
+        successMessage.innerText = "Your request was successfully submitted!";
+        successMessage.style.color = "green";
+        successMessage.style.padding = "15px";
+        successMessage.style.margin = "10px 0";
+        successMessage.style.border = "1px solid green";
+        successMessage.style.borderRadius = "5px";
+
+        // Get the form and insert the success message after it
+        var form = document.getElementById("cp-appraisal-form");
+        form.parentNode.insertBefore(successMessage, form.nextSibling);
+
+        // Clear form fields
+        document.getElementById('first_name').value = '';
+        document.getElementById('last_name').value = '';
+        document.getElementById('email').value = '';
+        document.getElementById('phone').value = '';
+
+        // Close the modal after 3 seconds
+        setTimeout(function () {
+            toggleModal();
+        }, 3000);
+    }
+
+    var url = 'https://app.carpraze.com/api/v1/iframe/customer/input';
+
+    function handleFormSubmission(event) {
+        event.preventDefault();
+
+        // Perform form validation
+        var first_name = document.getElementById('first_name').value;
+        var last_name = document.getElementById('last_name').value;
+        var email = document.getElementById('email').value;
+        var phone = document.getElementById('phone').value;
+
+        // Send form data to the server
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', url, true);
+        xhr.onload = handleFormOnload;
+
+        var data = new FormData();
+        data.append('token', window.carprazeForm.token);
+        data.append('first_name', first_name);
+        data.append('last_name', last_name);
+        data.append('email', email);
+        data.append('phone', phone);
+        data.append('form_type', 'customerInput');
+        xhr.send(data);
+    }
+
+    function toggleModal() {
+        var modal = document.getElementById('cp-appraisal-modal');
+        modal.classList.toggle("cp-show-modal");
+    }
+
+
     // Attach event listeners for form submission and modal toggling
     document.querySelector(window.carprazeForm.selector).addEventListener('click', toggleModal);
     document.querySelector('.cp-close-button').addEventListener('click', toggleModal);
     document.getElementById('cp-appraisal-form').addEventListener('submit', handleFormSubmission);
 })();
-
-function handleFormSubmission(event) {
-    event.preventDefault();
-
-    // Perform form validation
-    var first_name = document.getElementById('first_name').value;
-    var last_name = document.getElementById('last_name').value;
-    var email = document.getElementById('email').value;
-    var phone = document.getElementById('phone').value;
-
-    if (!first_name || !last_name || !email || !phone) {
-        alert('All fields are required!');
-        return false;
-    }
-
-    // Send form data to the server
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'https://app.carpraze.com/api/v1/iframe/customer/input', true);
-    xhr.onload = function () {
-        if (this.status === 200 || this.status === 201) {
-            var response = JSON.parse(this.responseText);
-            // Create a success message element
-            var successMessage = document.createElement("div");
-            successMessage.innerText = "Your request was successfully submitted!";
-            successMessage.style.color = "green";
-            successMessage.style.padding = "15px";
-            successMessage.style.margin = "10px 0";
-            successMessage.style.border = "1px solid green";
-            successMessage.style.borderRadius = "5px";
-
-            // Get the form and insert the success message after it
-            var form = document.getElementById("cp-appraisal-form");
-            form.parentNode.insertBefore(successMessage, form.nextSibling);
-
-            // Clear form fields
-            document.getElementById('first_name').value = '';
-            document.getElementById('last_name').value = '';
-            document.getElementById('email').value = '';
-            document.getElementById('phone').value = '';
-
-            // Close the modal after 3 seconds
-            setTimeout(function() {
-                toggleModal();
-            }, 3000);
-        } else {
-            console.error("There was an error with the request.");
-        }
-    };
-    var data = new FormData();
-    data.append('token', window.carprazeForm.token);
-    data.append('first_name', first_name);
-    data.append('last_name', last_name);
-    data.append('email', email);
-    data.append('phone', phone);
-    data.append('form_type', document.getElementById('form_type').value);
-    xhr.send(data);
-}
-
-function toggleModal() {
-    var modal = document.getElementById('cp-appraisal-modal');
-    modal.classList.toggle("cp-show-modal");
-}
-
